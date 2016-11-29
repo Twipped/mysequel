2871e0188d2dd8e6f455b9e541b26f66f575ed97
ChiperSoft <chiper@chipersoft.com>
2016-11-29 09:43:27 -0800

    Add query events to retry tests so we see life cycle events working.

--- tests/unit/retry.js
+++ tests/unit/retry.js
@@ -24,15 +24,19 @@ suite('promise-query', (s) => {
 
 	var db = quint({ ping: false });
 	db.getPool = makeMockPool(stepper);
+	db.on('query-start',   stepper.as('event:query-start'));
+	db.on('query-retry',   stepper.as('event:query-retry'));
+	db.on('query-success', stepper.as('event:query-success'));
+	db.on('query-error',   stepper.as('event:query-error'));
+	db.on('query-done',    stepper.as('event:query-done'));
 
 	s.beforeEach((done) => {
 		stepper.reset(true);
 		done();
 	});
 
-
 	s.test('query retry', (t) => {
-		t.plan(16);
+		t.plan(21);
 		var q = {
 			sql: 'QUERY',
 			values: { foo: 'bar' },
@@ -44,6 +48,10 @@ suite('promise-query', (s) => {
 			[ { id: '' } ],
 			[ { name: 'id' } ],
 		];
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-start', 'fired query-start');
+		});
 
 		stepper.add((method) => {
 			t.equal(method, 'pool.getConnection', 'called connection.query');
@@ -58,6 +66,10 @@ suite('promise-query', (s) => {
 
 		stepper.add((method) => {
 			t.equal(method, 'connection.release', 'called connection.release');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-retry', 'fired query-retry');
 		});
 
 		stepper.add((method) => {
@@ -76,6 +88,10 @@ suite('promise-query', (s) => {
 		});
 
 		stepper.add((method) => {
+			t.equal(method, 'event:query-retry', 'fired query-retry');
+		});
+
+		stepper.add((method) => {
 			t.equal(method, 'pool.getConnection', 'called connection.query');
 		});
 
@@ -90,13 +106,22 @@ suite('promise-query', (s) => {
 			t.equal(method, 'connection.release', 'called connection.release');
 		});
 
+		stepper.add((method) => {
+			t.equal(method, 'event:query-success', 'fired query-success');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-done', 'fired query-done');
+		});
+
+
 		return db.mockQuery(q).then((result) => {
 			t.deepEqual(result, expected, 'got back result');
 		});
 	});
 
 	s.test('query retry failure', (t) => {
-		t.plan(11);
+		t.plan(15);
 		var q = {
 			sql: 'QUERY',
 			values: { foo: 'bar' },
@@ -105,18 +130,7 @@ suite('promise-query', (s) => {
 		};
 
 		stepper.add((method) => {
-			t.equal(method, 'pool.getConnection', 'called connection.query');
-		});
-
-		stepper.add((method, query) => {
-			t.equal(method, 'mockQuery', 'called mockQuery');
-			t.equal(query.sql, 'QUERY', 'with the triggering query');
-			t.deepEqual(query.values, { foo: 'bar' }, 'and data');
-			return Promise.reject(new FatalError('ERROR'));
-		});
-
-		stepper.add((method) => {
-			t.equal(method, 'connection.release', 'called connection.release');
+			t.equal(method, 'event:query-start', 'fired query-start');
 		});
 
 		stepper.add((method) => {
@@ -135,6 +149,10 @@ suite('promise-query', (s) => {
 		});
 
 		stepper.add((method) => {
+			t.equal(method, 'event:query-retry', 'fired query-retry');
+		});
+
+		stepper.add((method) => {
 			t.equal(method, 'pool.getConnection', 'called connection.query');
 		});
 
@@ -147,6 +165,14 @@ suite('promise-query', (s) => {
 
 		stepper.add((method) => {
 			t.equal(method, 'connection.release', 'called connection.release');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-error', 'fired query-error');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-done', 'fired query-done');
 		});
 
 		return db.mockQuery(q).then(
@@ -155,14 +181,18 @@ suite('promise-query', (s) => {
 		);
 	});
 
-	s.only('query retry - non-fatal error', (t) => {
-		t.plan(6);
+	s.test('query retry - non-fatal error', (t) => {
+		t.plan(9);
 		var q = {
 			sql: 'QUERY',
 			values: { foo: 'bar' },
 			retry: true,
 			retryCount: 1,
 		};
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-start', 'fired query-start');
+		});
 
 		stepper.add((method) => {
 			t.equal(method, 'pool.getConnection', 'called connection.query');
@@ -177,6 +207,14 @@ suite('promise-query', (s) => {
 
 		stepper.add((method) => {
 			t.equal(method, 'connection.release', 'called connection.release');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-error', 'fired query-error');
+		});
+
+		stepper.add((method) => {
+			t.equal(method, 'event:query-done', 'fired query-done');
 		});
 
 		return db.mockQuery(q).then(
